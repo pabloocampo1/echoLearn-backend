5f8b210f5baf555b4203973cc87e91fe9eeb737b
pablo ocampo <juanpabloocampoleon@gmail.com>
2025-05-08 08:40:17 -0500

    feat(Auth): all configuration of security, roles and architecture (hexagonal)

--- src/main/java/com/EchoLearn_backend/infraestructure/adapter/entity/UserEntity.java
+++ src/main/java/com/EchoLearn_backend/infraestructure/adapter/entity/UserEntity.java
@@ -8,10 +8,16 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "user")
 @Data
+@EntityListeners(AuditingEntityListener.class)
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -35,6 +41,12 @@ public class UserEntity {
     private String email;
 
     private String photo;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime updateDate;
 
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "id_profile", referencedColumnName = "id_profile")
